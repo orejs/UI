d652ff8340483002c362deed9ac3aba03cc54120
sengmitnick <sengmitnick@163.com>
2022-04-20 09:51:46 +0000

    feat: mvp done

--- config/config.ts
+++ config/config.ts
@@ -7,5 +7,9 @@ export default defineConfig({
   logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   outputPath: 'docs-dist',
   mode: 'site',
+  alias: {
+    'ored/es': process.cwd() + '/src',
+    demos: process.cwd() + '/src/demos/index.ts',
+  },
   // more config: https://d.umijs.org/config
 });
